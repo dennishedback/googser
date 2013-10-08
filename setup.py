d842648c79db475092ed6ca6ff96cd548618ed28
d.hedback@gmail.com <d.hedback@gmail.com>
2013-10-08 21:44:01 +0200

    Add installation script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "googser",
+    version = "0.1.0",
+    description = "Search google using the command line",
+    author = "Dennis Hedback",
+    author_email = "d.hedback@gmail.com",
+    scripts = ["script/googser"],
+)
